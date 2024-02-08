8a7e446da7d3cfad3c70cba6d2fa4ba5f106a0b2
biohzrdmx <hi@biohzrdmx.me>
2024-02-07 18:48:46 -0600

    Fixed race condition in DatabaseAdapter, also added configurable table and column name

--- src/Queue/Adapter/DatabaseAdapter.php
+++ src/Queue/Adapter/DatabaseAdapter.php
@@ -31,6 +31,12 @@ class DatabaseAdapter extends AbstractAdapter {
     protected $table = 'queue';
 
     /**
+     * Column name
+     * @var string
+     */
+    protected $column = 'uid';
+
+    /**
      * Database instance
      * @var Database
      */
@@ -40,8 +46,10 @@ class DatabaseAdapter extends AbstractAdapter {
      * Constructor
      * @param Database $database Database instance
      */
-    public function __construct(Database $database) {
+    public function __construct(Database $database, string $table = 'queue', string $column = 'uid') {
         $this->database = $database;
+        $this->table = $table;
+        $this->column = $column;
         $this->createTable();
     }
 
@@ -53,9 +61,10 @@ class DatabaseAdapter extends AbstractAdapter {
      */
     public function add(string $type, $data): string {
         $ret = null;
+        $uid = $this->generateUID();
         $row = [
             'id' => 0,
-            'uid' => '',
+            $this->column => $uid,
             'type' => $type,
             'data' => serialize($data),
             'status' => 'Pending',
@@ -66,11 +75,6 @@ class DatabaseAdapter extends AbstractAdapter {
         $query = new Query($this->database);
         $id = $query->table($this->table)->insert($row);
         if ($id) {
-            $uid = $this->generateUID();
-            $query = new Query($this->database);
-            $query->table($this->table)
-                ->where('id', $id)
-                ->update(['uid' => $uid]);
             $ret = $uid;
         }
         return $ret;
@@ -114,7 +118,7 @@ class DatabaseAdapter extends AbstractAdapter {
     public function complete(string $uid): void {
         $query = new Query($this->database);
         $query->table($this->table)
-            ->where('uid', $uid)
+            ->where($this->column, $uid)
             ->delete();
     }
 
@@ -126,7 +130,7 @@ class DatabaseAdapter extends AbstractAdapter {
     public function failed(string $uid): void {
         $query = new Query($this->database);
         $query->table($this->table)
-            ->where('uid', $uid)
+            ->where($this->column, $uid)
             ->update(['status' => 'Failed']);
     }
 
@@ -138,7 +142,7 @@ class DatabaseAdapter extends AbstractAdapter {
     public function retry(string $uid): void {
         $query = new Query($this->database);
         $query->table($this->table)
-            ->where('uid', $uid)
+            ->where($this->column, $uid)
             ->update(['status' => 'Pending', 'retries' => Argument::raw('retries + 1')]);
     }
 
@@ -195,14 +199,14 @@ class DatabaseAdapter extends AbstractAdapter {
         if (! $schema->hasTable($this->table) ) {
             $schema->create($this->table, function($table) {
                 $table->bigInteger('id')->autoIncrement();
-                $table->string('uid', 180);
+                $table->string($this->column, 180);
                 $table->mediumText('data');
                 $table->string('type', 50);
                 $table->string('status', 50);
                 $table->integer('retries');
                 $table->dateTime('created');
                 $table->dateTime('modified');
-                $table->index('key_uid', 'uid');
+                $table->index('key_uid', $this->column);
                 $table->index('key_type', 'type');
                 $table->index('key_status', 'status');
                 $table->primary('pk_id', 'id');
